5034ad8b17b0a27736943b81c46495afe117f1b6
Oliver George <oliver@condense.com.au>
2020-07-10 12:35:03 +1000

    Repro for reframe issue: Improve exception handing associated with subs
    https://github.com/day8/re-frame/issues/633

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,3 +1,22 @@
-(ns hello-world.core)
+(ns hello-world.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
+            [re-frame.core :as rf]))
 
-(println "Hello world!")
+(rf/reg-sub
+  ::throwing-sub
+  (fn [_ _]
+    (assert (odd? 2))
+    "fn sub"))
+
+(defn app []
+  [:p @(rf/subscribe [::throwing-sub])])
+
+(defn error-boundary []
+  (let [*error (r/atom nil)]
+    (r/create-class
+      {:get-derived-state-from-error (fn [e] (reset! *error e))
+       :component-did-catch          (fn [_ _ _] true)
+       :reagent-render               (fn [form] (if @*error [:div (str @*error)] form))})))
+
+(rdom/render [error-boundary [app]] (js/document.getElementById "app"))
